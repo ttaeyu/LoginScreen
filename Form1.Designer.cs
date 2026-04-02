02987190b05d64f2f28cfc97375f082e40124202
user <gjxodb1458@gmail.com>
2026-04-02 17:10:31 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace LoginScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("맑은 고딕", 27.75F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            btnLogin.Location = new Point(80, 353);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(211, 82);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += button1_Click;
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("맑은 고딕", 48F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblAppName.Location = new Point(80, 9);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(198, 86);
+            lblAppName.TabIndex = 1;
+            lblAppName.Text = "Login";
+            lblAppName.Click += label1_Click;
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtID.ForeColor = SystemColors.ScrollBar;
+            txtID.Location = new Point(40, 137);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(330, 43);
+            txtID.TabIndex = 2;
+            txtID.Text = "아이디";
+            txtID.TextChanged += textBox1_TextChanged;
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("맑은 고딕", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            txtPW.ForeColor = SystemColors.ScrollBar;
+            txtPW.Location = new Point(40, 203);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(330, 43);
+            txtPW.TabIndex = 3;
+            txtPW.Text = "패스워드";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(397, 447);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Controls.Add(btnLogin);
+            Name = "Form1";
+            Text = "Login Screen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        string myID = "sungmin";
+        string myPW = "superman";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string inputID = txtID.Text;
+            string inputPW = txtPW.Text;
+            if (inputID == myID && inputPW == myPW)
+            {
+                MessageBox.Show( "로그인 성공");
+            }
+            else
+            {
+                // 틀리면 가차 없이 쫓아내부러야제!
+                MessageBox.Show( "로그인 실패");
+            }
+        }
+
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디")
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "패스워드")
+            {
+                txtPW.Text = "";
+                txtPW.ForeColor = Color.Black;
+                txtPW.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPW.Text))
+            {
+                txtPW.Text = "패스워드";
+                txtPW.ForeColor = Color.Gray;
+                txtPW.UseSystemPasswordChar = false;
+            }
+        }
+    }
+}
+
+
+
